3e5969bc626a675b1b71e0020832868b27d12e17
Lev Kruglyak <levkruglyak@dhcp-10-250-150-162.harvard.edu>
2024-03-11 14:43:42 -0400

    update

---
+++ CS181/pset3/problem3.py
@@ -0,0 +1,9 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from T3_P3_TestCases import test_forward_pass
+
+%matplotlib inline
+%config InlineBackend.figure_format = 'svg'
